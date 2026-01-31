7f7a59128fb2e07dae82da1bb9e5e6411658d4e4
Alex Onceanu <4lex.onceanu@gmail.com>
2026-01-31 12:56:59 +0100

    weird spin but ok

--- Scripts/Couple.cs
+++ Scripts/Couple.cs
@@ -3,9 +3,14 @@ using System;
 
 public partial class Couple : Path3D
 {
+	private const float SPIN_DURATION = 0.8f;
 	[Export] PathFollow3D anchor;
+    [Export] Dancer dancer1;
+    [Export] Dancer dancer2;
 
     float elapsed;
+	private Vector3 center => (dancer1.Position + dancer2.Position) * 0.5f;
+    private const float dancerDist = 0.13f;
     [Export] float moveSpeed = .3f;
     float distanceEachStepInMeter = .4f;
     float distanceToParkour;
@@ -15,10 +20,26 @@ public partial class Couple : Path3D
     float startProgress;
     float endProgress;
 
+    private void SetRotation(float tp)
+    {
+        float t = tp;
+        dancer1.Position = new Vector3(dancerDist * Mathf.Cos(Mathf.Tau * t), 0.0f, dancerDist * Mathf.Sin(Mathf.Tau * t));
+        dancer2.Position = new Vector3(dancerDist * Mathf.Cos(Mathf.Tau * t + Mathf.Pi), 0.0f, dancerDist * Mathf.Sin(Mathf.Tau * t + Mathf.Pi));
+    }
+
+    private void Spin()
+    {
+        dancer1.Spin();
+        dancer2.Spin();
+        Callable callmdr = new Callable(this, nameof(SetRotation));
+        GetTree().CreateTween().TweenMethod(callmdr, 0.0f, 1.0f, SPIN_DURATION);
+    }
+
     public override void _Ready()
     {
         base._Ready();
         action = ListenInput;
+        dancer2.Flip();
     }
 
     public override void _Process(double delta)
@@ -32,6 +53,11 @@ public partial class Couple : Path3D
         if (Input.IsActionJustPressed("tap"))
         {
             StartPlayStep();
+        }
+
+        if (Input.IsActionJustPressed("spin"))
+        {
+            Spin();
         }
     }
 
