9ba785af8d57ef61665d78fe7e2fee90fedcfb35
Eliott <tan.eliott04@gmail.com>
2026-01-31 16:18:57 +0100

    add : responsabilities seperation

--- Scripts/Couple.cs
+++ Scripts/Couple.cs
@@ -3,14 +3,9 @@ using System;
 
 public partial class Couple : Path3D
 {
-	private const float SPIN_DURATION = 0.8f;
-	[Export] PathFollow3D anchor;
-    [Export] Dancer dancer1;
-    [Export] Dancer dancer2;
-    [Export] Timer endRotationTimer;
+    [Export] PathFollow3D anchor;
 
     float elapsed;
-	private Vector3 center => (dancer1.Position + dancer2.Position) * 0.5f;
     private const float dancerDist = 0.13f;
     [Export] float moveSpeed = .3f;
     float distanceEachStepInMeter = .4f;
@@ -22,36 +17,11 @@ public partial class Couple : Path3D
     float endProgress;
     float rotationOffset = 0.35f;
 
-    private void SetRotation(float tp)
-    {
-        float t = tp + rotationOffset;
-        dancer1.Position = new Vector3(dancerDist * Mathf.Cos(Mathf.Pi * t), 0.0f, dancerDist * Mathf.Sin(Mathf.Pi * t));
-        dancer2.Position = new Vector3(dancerDist * Mathf.Cos(Mathf.Pi * t + Mathf.Pi), 0.0f, dancerDist * Mathf.Sin(Mathf.Pi * t + Mathf.Pi));
-    }
-
-    private void EndRotation()
-    {
-        SetRotation(1.0f);
-        rotationOffset += 1.0f;
-    }
-
-    private void Spin()
-    {
-        bool clockwise = Mathf.FloorToInt(rotationOffset) % 2 == 0;
-        dancer1.Spin(clockwise);
-        dancer2.Spin(!clockwise);
-        Callable callmdr = new Callable(this, nameof(SetRotation));
-        GetTree().CreateTween().TweenMethod(callmdr, 0.0f, 1.0f, SPIN_DURATION);
-        endRotationTimer.Start();
-    }
 
     public override void _Ready()
     {
         base._Ready();
         action = ListenInput;
-        dancer1.Flip();
-        SetRotation(0.0f);
-        endRotationTimer.WaitTime = SPIN_DURATION;
     }
 
     public override void _Process(double delta)
@@ -62,14 +32,9 @@ public partial class Couple : Path3D
     void SetListenInput() { action = ListenInput; }
     void ListenInput(double delta)
     {
-        if (Input.IsActionJustPressed("tap"))
-        {
-            StartPlayStep();
-        }
-
         if (Input.IsActionJustPressed("spin"))
         {
-            Spin();
+            StartPlayStep();
         }
     }
 
@@ -81,8 +46,9 @@ public partial class Couple : Path3D
         elapsed = 0;
         action = PlayStep;
     }
-    void PlayStep(double delta){
-        float lerp = Mathf.Pow(elapsed,2) / moveSpeed;
+    void PlayStep(double delta)
+    {
+        float lerp = Mathf.Pow(elapsed, 2) / moveSpeed;
         anchor.Progress = Mathf.Lerp(startProgress, endProgress, lerp);
         elapsed += (float)delta;
         if (lerp >= 1)
@@ -93,5 +59,5 @@ public partial class Couple : Path3D
         }
     }
 
-    
+
 }
---
+++ Scripts/CoupleAnimator.cs
@@ -0,0 +1,79 @@
+using Godot;
+using System;
+
+public partial class CoupleAnimator : PathFollow3D
+{
+    private const float SPIN_DURATION = 0.8f;
+    [Export] Dancer dancer1;
+    [Export] Dancer dancer2;
+    [Export] Timer endRotationTimer;
+
+    float elapsed;
+    private Vector3 center => (dancer1.Position + dancer2.Position) * 0.5f;
+    private const float dancerDist = 0.13f;
+    [Export] float moveSpeed = .3f;
+    float distanceEachStepInMeter = .4f;
+    float distanceToParkour;
+
+    float startProgress;
+    float endProgress;
+    float rotationOffset = 0.35f;
+
+    bool spinning = false;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        dancer1.Flip();
+        SetRotation(0.0f);
+        endRotationTimer.WaitTime = SPIN_DURATION;
+    }
+
+    public override void _Process(double delta)
+    {
+        ListenInput(delta);
+    }
+
+    void ListenInput(double delta)
+    {
+        if (Input.IsActionJustPressed("spin"))
+        {
+            Spin();
+
+        }
+    }
+    private void SetRotation(float tp)
+    {
+        float t = tp + rotationOffset;
+        dancer1.Position = new Vector3(dancerDist * Mathf.Cos(Mathf.Pi * t), 0.0f, dancerDist * Mathf.Sin(Mathf.Pi * t));
+        dancer2.Position = new Vector3(dancerDist * Mathf.Cos(Mathf.Pi * t + Mathf.Pi), 0.0f, dancerDist * Mathf.Sin(Mathf.Pi * t + Mathf.Pi));
+    }
+
+    private void EndRotation()
+    {
+        SetRotation(1.0f);
+        rotationOffset += 1.0f;
+        GD.Print(rotationOffset);
+    }
+
+    private void Spin()
+    {
+        if (spinning)
+            return;
+        spinning = true;
+        bool clockwise = Mathf.FloorToInt(rotationOffset) % 2 == 0;
+        dancer1.Spin(clockwise);
+        dancer2.Spin(!clockwise);
+        Callable SetRotation = new Callable(this, nameof(SetRotation));
+        Tween tween = GetTree().CreateTween();
+        tween.TweenMethod(SetRotation, 0.0f, 1.0f, SPIN_DURATION);
+        endRotationTimer.Start();
+        tween.TweenCallback(new Callable(this,nameof(ResetSpin)));
+        GD.Print("Spining started");
+    }
+
+    private void ResetSpin()
+    {
+        spinning = false;
+    }
+}
