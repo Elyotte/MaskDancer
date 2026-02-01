d33530b622d09d2953cdf82cf46735428bfd028b
Alex Onceanu <4lex.onceanu@gmail.com>
2026-02-01 03:21:23 +0100

    Fixed input couple

--- Scripts/Couple.cs
+++ Scripts/Couple.cs
@@ -3,75 +3,104 @@ using System;
 
 public partial class Couple : Path3D
 {
-    [Export] PathFollow3D anchor;
+	[Export] PathFollow3D anchor;
+	[Export] float rythm = 1.2f;
+	[Export] Label3D textLabel;
 
-    float stepDuration = 0.7f;
-    float elapsed;
-    private const float dancerDist = 0.13f;
-    [Export] float moveSpeed = .3f;
-    float distanceEachStepInMeter = .4f;
-    float distanceToParkour;
+	float stepDuration = 0.7f;
+	float elapsed;
+	private const float dancerDist = 0.13f;
+	[Export] float moveSpeed = .3f;
+	float distanceEachStepInMeter = .4f;
+	float distanceToParkour;
 
-    Action<double> action;
-    Tween currentTween;
+	Action<double> action;
+	Tween currentTween;
 
-    float startProgress;
-    float endProgress;
-    float rotationOffset = 0.35f;
+	float startProgress;
+	float endProgress;
+	float rotationOffset = 0.35f;
+	
+	
+	public override void _Ready()
+	{
+		CoupleManager.AddCouple(this);
+		base._Ready();
+		action = ListenInput;
+		GetNode<Timer>("LabelDisappear").WaitTime = 0.85f * rythm;
 
+	}
 
-    public override void _Ready()
-    {
-        CoupleManager.AddCouple(this);
-        base._Ready();
-        action = ListenInput;
-    }
+	public override void _Process(double delta)
+	{
+		action?.Invoke(delta);
+	}
 
-    public override void _Process(double delta)
-    {
-        action?.Invoke(delta);
-    }
+	private void SetTimingProgression(float t)
+	{
+		var shaderMaterial = (ShaderMaterial)GetNode<Sprite3D>("Sprite3D").MaterialOverride;
+		shaderMaterial.SetShaderParameter("t", t);
+	}
 
-    void SetListenInput() { 
-        action = ListenInput; 
-    
-    }
-    void ListenInput(double delta)
-    {
-        if (Input.IsActionJustPressed("spin"))
-        {
-            StartPlayStep();
-        }
-    }
+	public void Appear(string letter)
+	{
+		textLabel.Text = letter;
+		Visible = true;
+		SetTimingProgression(0.0f);
+		Callable callableTimingProgression = new Callable(this, nameof(SetTimingProgression));
+		GetTree().CreateTween().TweenMethod(callableTimingProgression, 0.0f, 1.0f, rythm);
+		GetTree().CreateTween().TweenProperty(textLabel, "modulate:a", 1.0f, 0.15f * rythm);
+		GetTree().CreateTween().TweenProperty(textLabel, "outline_modulate:a", 1.0f, 0.15f * rythm);
+		GetNode<Timer>("LabelDisappear").Start();
+	}
+	
+	private void MakeLabelDisappear()
+	{
+		GetTree().CreateTween().TweenProperty(textLabel, "modulate:a", 0.0f, 0.15f * rythm);
+		GetTree().CreateTween().TweenProperty(textLabel, "outline_modulate:a", 0.0f, 0.15f * rythm);
+	}
 
-    void StartPlayStep()
-    {
-        action = null;
+	void SetListenInput() { 
+		action = ListenInput; 
+	
+	}
+	void ListenInput(double delta)
+	{
+		if (Input.IsActionJustPressed("spin"))
+		{
+			StartPlayStep();
+			Appear("Y");
+		}
+	}
 
-        startProgress = anchor.Progress;
-        endProgress = startProgress + distanceEachStepInMeter;
-        
-        currentTween = GetTree().CreateTween();
-        currentTween.TweenProperty(anchor, "progress", endProgress, stepDuration).SetEase(Tween.EaseType.InOut).SetTrans(Tween.TransitionType.Circ);
+	void StartPlayStep()
+	{
+		action = null;
 
-        currentTween.Finished += SetListenInput;
-    }
-    void PlayStep(double delta)
-    {
-        float lerp = Mathf.Pow(elapsed, 2) / moveSpeed;
-        anchor.Progress = Mathf.Lerp(startProgress, endProgress, lerp);
-        elapsed += (float)delta;
-        if (lerp >= 1)
-        {
-            anchor.Progress = endProgress;
-            elapsed = 0;
-            SetListenInput();
-        }
-    }
+		startProgress = anchor.Progress;
+		endProgress = startProgress + distanceEachStepInMeter;
+		
+		currentTween = GetTree().CreateTween();
+		currentTween.TweenProperty(anchor, "progress", endProgress, stepDuration);
 
-    public override void _ExitTree()
-    {
-        CoupleManager.RemoveCouple(this);
-        base._ExitTree();
-    }
+		currentTween.Finished += SetListenInput;
+	}
+	void PlayStep(double delta)
+	{
+		float lerp = Mathf.Pow(elapsed, 2) / moveSpeed;
+		anchor.Progress = Mathf.Lerp(startProgress, endProgress, lerp);
+		elapsed += (float)delta;
+		if (lerp >= 1)
+		{
+			anchor.Progress = endProgress;
+			elapsed = 0;
+			SetListenInput();
+		}
+	}
+
+	public override void _ExitTree()
+	{
+		CoupleManager.RemoveCouple(this);
+		base._ExitTree();
+	}
 }
